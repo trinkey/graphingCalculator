ed921fa826894f2d6ec4d483b20867ed2362c939
trinkey_ <97406176+trinkey@users.noreply.github.com>
2022-09-01 12:12:23 -0400

    Create grapher.py

---
+++ grapher.py
@@ -0,0 +1,52 @@
+step = 0.1
+
+screenWidth = 600 # Any Integer
+screenHeight = 400 # Any Integer
+
+import turtle, numpy, math
+x = 0 - (screenWidth / 2)
+
+screen = turtle.Screen()
+screen.setup(screenWidth, screenHeight)
+screen.bgcolor("#333333")
+screen.tracer(0)
+
+turtle1 = turtle.Turtle()
+turtle1.color("deep sky blue")
+turtle1.pu()
+turtle1.speed(0)
+turtle1.goto(-100, 0)
+
+turtle2 = turtle.Turtle()
+turtle2.color("medium spring green")
+turtle2.pu()
+turtle2.speed(0)
+turtle2.goto(-100, 0)
+
+turtle3 = turtle.Turtle()
+turtle3.color("orchid")
+turtle3.pu()
+turtle3.speed(0)
+turtle3.goto(-100, 0)
+
+for i in range(round(screenWidth / step)):
+    equasion1 = 0.01 * (x ** 3) + (0.5 * x)
+    equasion3 = 0
+    equasion2 = -x
+    
+    turtle1.goto(x, equasion1)
+    turtle1.pd()
+    
+    turtle2.goto(x, equasion2)
+    turtle2.pd()
+    
+    turtle3.goto(x, equasion3)
+    turtle3.pd()
+    
+    x += step
+
+turtle1.hideturtle()
+turtle2.hideturtle()
+turtle3.hideturtle()
+
+screen.update()
