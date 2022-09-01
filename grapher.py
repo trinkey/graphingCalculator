768b875352e25b57ebbd40e6fb7020896e3e3d2c
trinkey_ <97406176+trinkey@users.noreply.github.com>
2022-09-01 15:00:31 -0400

    added modes + more
    modes (0, 1, 2)
    equations are now at the top of the program, along with the boys
    step is now defaulted to 1 because for some reason that looks better

--- grapher.py
+++ grapher.py
@@ -1,4 +1,14 @@
-step = 0.1
+equation1, equation2, equation3 = 0, 0, 0 # Don't mind this line, it serves no purpse to you and changing it will do nothing
+
+step = 1 # How visually accurate the graph is - I suggest keeping it at 1 unless you need to chage it othewise
+mode = 1 # Modes: 0 - input/output; 1 - graph only; 2 - both
+
+# Imagine the equasion# = as a y =
+def equations(x): # Define the equasions here. If you dont want one, just set it to 0 and it will meka a straight line at y = 0 
+    global equation1, equation2, equation3
+    equation1 = 0.005 * x ** 3
+    equation2 = math.floor(x / 15) * 10
+    equation3 = 86.6
 
 screenWidth = 600 # Any Integer
 screenHeight = 400 # Any Integer
@@ -29,24 +39,31 @@ turtle3.pu()
 turtle3.speed(0)
 turtle3.goto(-100, 0)
 
-for i in range(round(screenWidth / step)):
-    equation1 = 0.001 * (x ** 3) + x
-    equation2 = math.floor(x / 15) * 15
-    equation3 = 0.003 * x ** 2 - (screenHeight / 2)
-    
-    turtle1.goto(x, equation1)
-    turtle1.pd()
-    
-    turtle2.goto(x, equation2)
-    turtle2.pd()
-    
-    turtle3.goto(x, equation3)
-    turtle3.pd()
-    
-    x += step
+if mode == 1 or mode == 2:
+    for i in range(round(screenWidth / step)):
+        equations(x)
+        
+        turtle1.goto(x, equation1)
+        turtle1.pd()
+        
+        turtle2.goto(x, equation2)
+        turtle2.pd()
+        
+        turtle3.goto(x, equation3)
+        turtle3.pd()
+        
+        x += step
 
 turtle1.hideturtle()
 turtle2.hideturtle()
 turtle3.hideturtle()
 
 screen.update()
+
+if mode == 0 or mode == 2:
+    while True:
+        try:
+          x = input("What x coordinate do you wanna find the y values for? (decimals supported)")
+          equations(float(x))
+          print("Y values when x = " + x + ":\nEquation 1: " + str(equation1) + "\nEquation 2: " + str(equation2) + "\nEquation 3: " + str(equation3))
+        except: print("Bad input")
