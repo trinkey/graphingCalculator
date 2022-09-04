7ad6e486dc30b8da0b06efd5d261c6ab81d09e00
trinkey_ <97406176+trinkey@users.noreply.github.com>
2022-09-03 21:22:11 -0400

    time to write the patch notes rq
    added a 4th equation
    no longer error for negative square root on Mac (hopefully)
    no longer error when mode = 0 because undefined variable (me being dumb lmao)

--- grapher.py
+++ grapher.py
@@ -1,28 +1,42 @@
 step = 0.1 # How visually accurate the graph is - I suggest keeping it at 0.1 unless you need to chage it othewise
 
-mode = 2 # Modes: 0 - input/output; 1 - graph only; 2 - both
+mode = 1 # Modes: 0 - input/output; 1 - graph only; 2 - both
 
-screenWidth = 600 # Any Integer
-screenHeight = 400 # Any Integer
+screenWidth = 1000 # Any Integer
+screenHeight = 750 # Any Integer
+
 
 # Imagine the equasion# = as a y =
 def equations(x): # Define the equasions here. If you dont want one, just set it to 0 and it will meka a straight line at y = 0 
-    global equation1, equation2, equation3, equation1string, equation2string, equation3string
-    equation1 = math.floor(x / 15) * 10
-    equation2 = 0.003 * x ** 2 - (screenHeight / 2) + 10
-    equation3 = 10 * math.sqrt(x)
+    global equation1, equation2, equation3, equation4, equation1string, equation2string, equation3string, equation4string, screenWidth, screenHeight, mode
+    try: equation1 = math.floor(x / 15) * 10
+    except: equation1 = float("nan") # ignore this line
+    try: equation2 = 0.003 * x ** 2 - (screenHeight / 2) + 10
+    except: equation2 = float("nan") # and this one
+    try: equation3 = 100 * math.sin(x / 10)
+    except: equation3 = float("nan") # and this
+    try: equation4 = 10 * math.sqrt(-x)
+    except: equation4 = float("nan") # and finally, this
     
     # You don't need to do anything below this line
     
-    if math.isnan(float(equation1)): turtle1.pu(); equation1 = 0; equation1string = "NaN"
-    if math.isnan(float(equation2)): turtle2.pu(); equation2 = 0; equation2string = "NaN"
-    if math.isnan(float(equation3)): turtle3.pu(); equation3 = 0; equation3string = "NaN"
+    try:
+        if math.isnan(float(equation1)): turtle1.pu()
+        if math.isnan(float(equation2)): turtle2.pu()
+        if math.isnan(float(equation3)): turtle3.pu()
+        if math.isnan(float(equation4)): turtle4.pu()
+    except: pass
+
+    if math.isnan(float(equation1)): equation1 = 0; equation1string = "NaN"
+    if math.isnan(float(equation2)): equation2 = 0; equation2string = "NaN"
+    if math.isnan(float(equation3)): equation3 = 0; equation3string = "NaN"
+    if math.isnan(float(equation4)): equation4 = 0; equation4string = "NaN"
 
 import turtle, math
 try: import numpy
 except: print("The numpy library is not installed. Please consider installing it.")
 
-equation1, equation2, equation3 = 0, 0, 0
+equation1, equation2, equation3, equation4 = 0, 0, 0, 0
 
 x = 0 - (screenWidth / 2)
 
@@ -48,6 +62,11 @@ if mode == 1 or mode == 2:
     turtle3.pu()
     turtle3.speed(0)
     
+    turtle4 = turtle.Turtle()
+    turtle4.color("gold")
+    turtle4.pu()
+    turtle4.speed(0)
+    
     for i in range(round(screenWidth / step)):
         equations(x)
         
@@ -57,27 +76,39 @@ if mode == 1 or mode == 2:
         turtle2.goto(x, equation2)
         turtle2.pd()
         
+
         turtle3.goto(x, equation3)
         turtle3.pd()
         
+        turtle4.goto(x, equation4)
+        turtle4.pd()
+        
         x += step
 
-turtle1.hideturtle()
-turtle2.hideturtle()
-turtle3.hideturtle()
+        turtle1.hideturtle()
+        turtle2.hideturtle()
+        turtle3.hideturtle()
+        turtle4.hideturtle()
 
-screen.update()
+try: screen.update()
+except: pass
 
 if mode == 0 or mode == 2:
     while True:
         try:
-          equation1string, equation2string, equation3string = "", "", ""
-          x = input("What x coordinate do you wanna find the y values for? (decimals supported)")
-          equations(float(x))
-          if equation1string == "": equation1string = str(equation1)
-          if equation2string == "": equation2string = str(equation2)
-          if equation3string == "": equation3string = str(equation3)
-          if mode == 2: print("Y values when x = " + x + ":\nEquation 1 (blue): " + equation1string + "\nEquation 2 (green): " + equation2string + "\nEquation 3 (purple): " + equation3string)
-          else: print("")("Y values when x = " + x + ":\nEquation 1 : " + equation1string + "\nEquation 2 : " + equation2string + "\nEquation 3 : " + equation3string)
+            equation1string, equation2string, equation3string, equation4string = "", "", "", ""
+            x = input("What x coordinate do you wanna find the y values for? (decimals supported)\n")
+            equations(float(x))
+
+            if equation1string == "": equation1string = str(equation1)
+            if equation2string == "": equation2string = str(equation2)
+            if equation3string == "": equation3string = str(equation3)
+            if equation4string == "": equation4string = str(equation4)
+            
+            if mode == 2: print("Y values when x = " + x + ":\nEquation 1 (blue): " + equation1string + "\nEquation 2 (green): " + equation2string + "\nEquation 3 (purple): " + equation3string+ "\nEquation 4 (purple): " + equation4string)
+            else: print("Y values when x = " + x + ":\nEquation 1 : " + equation1string + "\nEquation 2 : " + equation2string + "\nEquation 3 : " + equation3string+ "\nEquation 4 : " + equation4string)
+        
         except: print("Bad input")
-screen.mainloop()
+
+try: screen.mainloop()
+except: pass
