70aa4243d33502532838652b99284f0063eeda51
trinkey_ <97406176+trinkey@users.noreply.github.com>
2022-09-05 10:26:16 -0400

    added modes back and more comments

--- grapher.py
+++ grapher.py
@@ -1,9 +1,11 @@
 step = 0.1 # How visually accurate the graph is - I suggest keeping it at 0.1 unless you need to chage it othewise
 
-screenWidth = 600 # Any Integer
-screenHeight = 400 # Any Integer
+mode = 2 # Modes: 0 - input/output; 1 - graph only; 2 - both
 
-equations = [ # Define equations here. Make sure every one starts with 'lambda x: ' - treat that as a 'y = ' when writing equations
+screenWidth = 1000 # Any Integer
+screenHeight = 750 # Any Integer
+
+equations = [ # Define equations here. Make sure every one starts with 'lambda x: ' and if theres another equation after it it should have a comma after it - treat that as a 'y = ' when writing equations
     lambda x: math.floor(x / 15) * 10,
     lambda x: 0.003 * x ** 2 - (screenHeight / 2) + 10,
     lambda x: 100 * math.sin(x / 10),
@@ -12,53 +14,61 @@ equations = [ # Define equations here. Make sure every one starts with 'lambda x
 
 # You do not need to do anything below this line.
 
-import turtle, math
+# Import libraries
+import turtle, math, random
 try: import numpy
 except: print("The numpy library is not installed. Please consider installing it.")
 
-screen = turtle.Screen()
-screen.setup(screenWidth, screenHeight)
-screen.bgcolor("#333333")
-screen.tracer(0)
+graphers = [] # List for the class object
+turtleColors = ["#FF6347", "#FF8C00", "#FFD700", "#7CFC00", "#00FA9A", "#00CED1", "#6495ED", "#9370DB", "#DA70D6"] # Colors for the turtle
 
-graphers = []
-turtleColors = ["#FF6347", "#FF8C00", "#FFD700", "#7CFC00", "#00FA9A", "#00CED1", "#6495ED", "#9370DB", "#DA70D6"]
-x = 0 - (screenWidth / 2)
-
-class Graph:
-    def __init__(self, color, equation):
-        self.equation = equation
-        self.color = color
-        self.ycor = 0.0
-        self.turtle = turtle.Turtle()
-        self.turtle.color(self.color)
-        self.turtle.pu()
-        self.turtle.speed(0)
+class Graph: # Define the class
+    def __init__(self, color, equation, mode):
+        self.equation = equation # set the equation function
+        self.ycor = 0.0 # define the variable for later
+        if mode == 1 or mode == 2: # Define turtle if mode is 1 or 2
+            self.turtle = turtle.Turtle() # turtle object
+            self.turtle.color(color) # set turtle color
+            self.turtle.pu() # penup
+            self.turtle.speed(0) # maximum speed
     
-    def graphEquation(self, x):
-        self.ycor = self.equation(x)
-        if math.isnan(float(self.ycor)): self.turtle.pu()
-        else: self.turtle.goto(x, self.ycor)
-        self.turtle.pd()
+    def graphEquation(self, x): # What to do when graphing
+        self.ycor = self.equation(x) # Calculate y value
+        if math.isnan(float(self.ycor)): self.turtle.pu() # If output is nan then penup
+        else: self.turtle.goto(x, self.ycor) # If it isnt nan then goto correct coords
+        self.turtle.pd() # pendown
     
-    def inout(self, x): 
+    def inout(self, x):  # What to do when input/output
         try:
             self.vairbale = self.equation(float(x))
             if math.isnan(float(self.vairbale)): return "NaN"
             else: return self.vairbale
         except: return "Bad Input D:"
 
-for i in range(len(equations)): graphers.append(Graph(turtleColors[i % 9], equations[i]))
-for i in range(round(float(round(screenWidth / step)) + float(step))):
-    for o in graphers: o.graphEquation(x)
-    x += step
+if mode == 1 or mode == 2:
+    screen = turtle.Screen()
+    screen.setup(screenWidth, screenHeight)
+    screen.bgcolor("#333333")
+    screen.tracer(0)
 
-for o in graphers: o.turtle.hideturtle()
+for i in range(len(equations)): graphers.append(Graph(turtleColors[i % 9], equations[i], mode))
 
-screen.update()
+if mode == 1 or mode == 2:
+    x = 0 - (screenWidth / 2) # Set minimum x value
 
-while True:
-    x = input("What do you wanna find the y values for (decimals supported)?\n")
-    print("Outputs when x = " + x)
-    for o in range(len(graphers)): print("Equation " + str(o + 1) + ": " + str(graphers[o].inout(x)))
-    print()
+    for i in range(round(float(round(screenWidth / step)) + float(step))):
+        for o in graphers: o.graphEquation(x)
+        x += step
+    for o in graphers: o.turtle.hideturtle()
+
+    screen.update()
+
+if mode == 0 or mode == 2:
+    while True:
+        x = input("What do you wanna find the y values for (decimals supported)?\n")
+        print("Outputs when x = " + x)
+        for o in range(len(graphers)): print("Equation " + str(o + 1) + ": " + str(graphers[o].inout(x)))
+        print()
+
+if mode == 1:
+    screen.mainloop()
